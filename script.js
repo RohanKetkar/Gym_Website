5388b4de4338cea60909d43953a9352d97f54b7d
RohanKetkar <101008728+RohanKetkar@users.noreply.github.com>
2022-06-02 16:50:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// const newBtn=document.getElementById('newbtn');
+// newBtn.addEventListener('onclick',()=>{
+//     console.log('executed');
+//     location.href = "https://www.who.int/news-room/fact-sheets/detail/healthy-diet";
+// })
+
+//  newBtn('click',()=>{
+//  console.log('executed');
+//         location.href = "https://www.who.int/news-room/fact-sheets/detail/healthy-diet";
+       
+
+// });
+// newBtn();
+
+document. getElementById("newbtn"). onclick = function () {
+    location.href = "https://www.who.int/news-room/fact-sheets/detail/healthy-diet";
+
+    };
+
+    document.getElementById('btn'). onclick=function(){
+        location.href="https://www.webmd.com/fitness-exercise/features/7-most-effective-exercises";
+    }
+    document.getElementById('unique'). onclick=function(){
+        location.href="https://www.youtube.com/channel/UCmOJd9j2uys6HmPXVZfK_ag";
+    }
+
+   
